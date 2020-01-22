3e78909ff3d8ba54a4b19c7404e36a120a2192cd
roselandroche <r.landroche@gmail.com>
2020-01-21 19:55:38 -0500

    Ran migration, set up model

--- users/users-model.js
+++ users/users-model.js
@@ -1,8 +1,29 @@
 const bcrypt = require('bcryptjs')
 const db = require('../data/db-config')
 
-// functions
+function find() {
+    return db('users').select('id', 'username', 'department')
+}
+
+function findBy(filter) {
+    return db('users').where(filter)
+}
+
+async function add(user) {
+    user.password = await bcrypt.hash(user.password, 13)
+
+    const [id] = await db('users').insert(user)
+
+    return findById(id)
+}
+
+function findById(id) {
+    return db('users').where({ id }).first('id', 'username', 'department')
+}
 
 module.exports = {
-    // functions
+    find,
+    findBy,
+    add,
+    findById
 }
