b777394d7e09ad009013c4edf9539c5a6833d258
roselandroche <r.landroche@gmail.com>
2020-01-21 20:03:15 -0500

    Register is functional

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dbConfig = require('./data/db-config')
+const usersRouter = require('./users/users-router')
 
 const server = express()
 const port = process.env.PORT || 5000
 
 server.use(express.json())
+
+server.use('/api', usersRouter)
 
 server.use((err, req, res, next) => {
     console.log(`Err:`, err)
--- users/users-router.js
+++ users/users-router.js
@@ -1,9 +1,23 @@
 const bcrypt = require('bcryptjs')
 const express = require('express')
+const jwt = require('jsonwebtoken')
+// const restricted middleware
 
 const usersModel = require('./users-model')
+// const secrets = require('../data/db-config')
 
 const router = express.Router()
+
+router.post('/register', async (req, res, next) => {
+    try {
+        const saved = await usersModel.add(req.body)
+
+        res.status(201).json(saved)
+    }
+    catch (err) {
+        next(err)
+    }
+})
 
 // routes
 
