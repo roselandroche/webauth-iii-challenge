17161d4ea88bdb1ea9e74871593a768f01feaf92
roselandroche <r.landroche@gmail.com>
2020-01-21 20:18:37 -0500

    Log in functional

--- users/users-router.js
+++ users/users-router.js
@@ -1,10 +1,10 @@
 const bcrypt = require('bcryptjs')
 const express = require('express')
 const jwt = require('jsonwebtoken')
-// const restricted middleware
+const restricted = require('../middleware/restricted')
 
 const usersModel = require('./users-model')
-// const secrets = require('../data/db-config')
+const secrets = require('../config/secrets')
 
 const router = express.Router()
 
@@ -19,6 +19,33 @@ router.post('/register', async (req, res, next) => {
     }
 })
 
-// routes
+router.post('/login', async (req, res, next) => {
+    try {
+        const { username, password } = req.body
+        const user = await usersModel.findBy({ username }).first()
+        const passwordValid = await bcrypt.compare(password, user.password)
+
+        if(user && passwordValid) {
+            const token = jwt.sign({
+                subject: user.id,
+                user: user.username,
+            }, secrets.jwt, {
+                expiresIn: '7d',
+            })
+
+            res.status(200).json({
+                message: `Welcome ${user.username}!`,
+                token: token,
+            })
+        } else {
+            res.status(401).json({
+                message: `Invalid credentials`
+            })
+        }
+    }
+    catch(err) {
+        next(err)
+    }
+})
 
 module.exports = router
